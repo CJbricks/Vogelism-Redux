81af7f336d12c01fa17edf058653d2a7fa9768cc
C.J. Christian <118404402+CJbricks@users.noreply.github.com>
2023-03-16 12:15:46 -0600

    Added media Query for splash page.

--- vogelism/src/Splash.js
+++ vogelism/src/Splash.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import meme from './/images/memevogel.jpg';
 import './Splash.css';
+import memeLong from './/images/memelong.png';
 
 export default function Splash() {
     
     return (
-        <div>
-            <img 
-                src={meme}
-                alt="meme-logo"
-                className="meme"/>
-        </div>
+        <>
+            <div className="wide">
+                <img 
+                    src={meme}
+                    alt="front-page-logo"
+                    className="meme"/>
+            </div>
+            <div className="long">
+                <img
+                    src={memeLong}
+                    alt="media-front-page-logo"
+                    className="media-meme"
+                    />
+            </div>
+        </>
     )
 }
